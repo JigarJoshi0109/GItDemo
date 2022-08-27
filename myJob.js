0221da59d15be1877f4c6a076d39ea8065aab12f
JigarJoshi0109 <jigar.joshi0109@gmail.com>
2022-08-28 00:58:41 +0530

    callbacks

--- myJob.js
+++ myJob.js
@@ -1,3 +1,23 @@
-var num = 90;
-const inc = num+1;
-console.log(inc);
+
+
+
+//callbacks
+
+
+function callbacks(a,b){
+    setTimeout(() => {
+        console.log(a+b)
+    }, 1000);
+}
+
+callbacks(3,5);
+
+
+function x(y){
+console.log(x);
+y();
+}
+
+x(function y(){
+    console.log(y);
+})
